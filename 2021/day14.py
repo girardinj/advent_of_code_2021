ecd1250e178a30371cad081103661ceac4fceebd
Jarod Girardin <jarod.girardin@he-arc.ch>
2022-12-03 18:57:39 +0100

    modified day 14

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,5 +1,6 @@
 import collections
 import threading
+import time
 
 def get_data():
     s = '''
@@ -24,8 +25,8 @@ def get_data():
         '''
 
     rules = []
-    with open('day14_data.txt', 'r') as f:
-        s = f.read()
+    # with open('day14_data.txt', 'r') as f:
+    #     s = f.read()
     
     lines = s.strip().splitlines()
     polymer = Polymer_thing(lines.pop(0).strip())
@@ -35,6 +36,7 @@ def get_data():
         rules.append(Rule(line.strip()))
     
     return (polymer, rules)
+
 
 class Polymer_thing():
     def __init__(self, state):
@@ -57,7 +59,7 @@ class Polymer_thing():
             self.data.insert(k + counter, v)
             counter += 1
         self.to_morph = None
-    
+        
     def compute(self):
         chars = {}
         for c in self.data:
@@ -94,27 +96,86 @@ class Rule():
 def f(polymer, rule):
     polymer.prepare_to_morph(rule)
 
-def puzzle1(data):
+def puzzle1(data, with_threads=True):
     polymer, rules = data
 
     threads = []
-    for i in range(40):
+    time_start = time.time()
+    for i in range(10):
         print('step', i)
         for i in range(len(rules)):
-            threads.insert(i, threading.Thread(target = f, args=(polymer, rules[i])))
-            threads[i].start()
+            if with_threads:
+                threads.insert(i, threading.Thread(target = f, args=(polymer, rules[i])))
+                threads[i].start()
+            else:
+                polymer.prepare_to_morph(rules[i])
         for t in threads:
             t.join()
         threads = []
         polymer.morph()
-        
+    time_end = time.time()
     
     s = polymer.compute()
+    print('time:', time_end - time_start)
     return s
+
+def generate_thing(data):
+    pairs = {}
+    old_c = data.pop(0)
+    for c in data:
+        if pairs.get(old_c + c) is None:
+            pairs[old_c + c] = 1
+        else:
+            pairs[old_c + c] += 1
+        old_c = c
+    return pairs
+
+def puzzle2(data, n):
+    # data is stored like this:
+    # for NNCB:
+    # {'NN': 1, 'NC': 1, 'CB': 1}
+    # for NNCBCBN:
+    # {'NN': 1, 'NC': 1, 'CB': 2, 'BC': 1, 'BN': 1}
+    polymer, rules = data
+    data = polymer.data
+    pairs = generate_thing(data)
+
+    
+    pairs_to_add = []
+    for i in range(n):
+        # search if a pair exists
+        for rule in rules:
+            pair = rule.left + rule.right
+            if pairs.get(pair) is not None:
+                pairs_to_add.append(rule)
+        
+        # extends the thing
+        for rule in pairs_to_add:
+            pair = rule.left + rule.right
+            pairs[pair] -= 1
+            if pairs[pair] == 0:
+                del pairs[pair]
+            pair_left = rule.left + rule.middle
+            pair_right = rule.middle + rule.right
+            
+            if pairs.get(pair_left) is None:
+                pairs[pair_left] = 1
+            else:
+                pairs[pair_left] += 1
+            if pairs.get(pair_right) is None:
+                pairs[pair_right] = 1
+            else:
+                pairs[pair_right] += 1
+        
+        pairs_to_add = []
+    
+    print(pairs)
 
 def main():
     data = get_data()
-    print(puzzle1(data))
+    print(generate_thing([c for c in 'NBBBCNCCNBBNBNBBCHBHHBCHB']))
+    
+    print(puzzle2(data, 3))
 
 if __name__ == '__main__':
     main()
